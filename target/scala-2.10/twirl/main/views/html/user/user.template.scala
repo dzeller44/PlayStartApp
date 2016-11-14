189bd3818c9c68ce52f2b94242ac80d74970fb39
Dan Zeller <dzeller44@gmail.com>
2016-11-14 14:06:22 -0700

    Updates

--- target/scala-2.10/twirl/main/views/html/user/user.template.scala
+++ target/scala-2.10/twirl/main/views/html/user/user.template.scala
@@ -47,14 +47,14 @@ Seq[Any](_display_(/*1.2*/main()/*1.8*/ {_display_(Seq[Any](format.raw/*1.10*/("
 					<a class="services_item" href="/addprofile">
 						<p>
 							<img src=""""),_display_(/*18.19*/routes/*18.25*/.Assets.at("/img/icon_Add_Profile.png")),format.raw/*18.64*/(""""> <b>Add Business Profile</b>
-						</p> <span>Export Records...</span>
+						</p> <span>Add a new Business Profile...</span>
 					</a>
 				</div>
 				<div class="col-sm-3 margbot30">
 					<a class="services_item" href="/editprofile">
 						<p>
 							<img src=""""),_display_(/*25.19*/routes/*25.25*/.Assets.at("/img/icon_Edit_Profile.png")),format.raw/*25.65*/(""""> <b>Edit Business Profile</b>
-						</p> <span>Search Business Profiles...</span>
+						</p> <span>Edit your Business Profile(s)...</span>
 					</a>
 				</div>
 
@@ -89,10 +89,10 @@ Seq[Any](_display_(/*1.2*/main()/*1.8*/ {_display_(Seq[Any](format.raw/*1.10*/("
 object user extends user_Scope0.user
               /*
                   -- GENERATED --
-                  DATE: Mon Nov 14 11:44:34 MST 2016
+                  DATE: Mon Nov 14 14:04:09 MST 2016
                   SOURCE: C:/WebDev/git/PlayAuthenticate/app/views/user/user.scala.html
-                  HASH: 1ce83989be864615c22a1fceedd336cd176e035f
-                  MATRIX: 830->1|843->7|882->9|910->11|1337->411|1352->417|1412->456|1649->666|1664->672|1725->712
+                  HASH: 675e48e8fbc6538d70365b5a65dec335c8769a9c
+                  MATRIX: 830->1|843->7|882->9|910->11|1337->411|1352->417|1412->456|1661->678|1676->684|1737->724
                   LINES: 32->1|32->1|32->1|34->3|49->18|49->18|49->18|56->25|56->25|56->25
                   -- GENERATED --
               */
