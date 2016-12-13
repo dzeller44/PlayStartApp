2e419c0c76dac864d7b61ad3e12db04ad60cf7c8
Dan Zeller <dan.zeller@state.co.us>
2016-12-13 10:00:14 -0700

    Add SQL for testing

---
+++ INSERT_Profiles.sql
@@ -0,0 +1,40 @@
+INSERT INTO app.profile(
+   id
+  ,name
+  ,address
+  ,address1
+  ,city
+  ,state
+  ,zip
+  ,country
+  ,county
+  ,billname
+  ,billaddress
+  ,billaddress1
+  ,billcity
+  ,billstate
+  ,billzip
+  ,billcountry
+  ,billcounty
+  ,primary_name_first
+  ,primary_name_last
+  ,primary_phone
+  ,primary_email
+  ,secondary_name_first
+  ,secondary_name_last
+  ,secondary_phone
+  ,secondary_email
+  ,services
+  ,services_other
+  ,date_creation
+  ,profilekey
+  ,userkey
+  ,updated_by
+  ,date_updated
+  ,date_remind
+) VALUES 
+(NULL, 'Bobs Dumpsters', '8457 First Ave.', '', 'Denver', 'CO', '80203', 'United States', 'Clear Creek', 'Bob''s Dumpsters', '29874 Any Street', '', 'Denver', 'CO', '80206', 'United States', 'Adams', 'Bill', 'Williams', '3035551212', 'bw@google.com', 'John', 'Doe', '3035551212', 'jdoe@google.com', 'Dumpsters,Showers,Generators,Pumps', NULL, NULL, 'ff461c923781496fadccc0aa2086e610', '123456', NULL, NULL, NULL), 
+(NULL, 'Acme', '45534 Any Street', '', 'Denver', 'CO', '80213', 'United States', 'Broomfield', 'Acme', '989 Second Ave.', '', 'Denver', 'CO', '80206', 'United States', 'Kiowa', 'John', 'Jackson', '3035551212', 'jj@google.com', 'Jane', 'Doe', '3035551212', 'janedoe@google.com', 'Showers,Pumps,Sandbags', NULL, NULL, '987fad6tsGHIU89YRAIHGBFJD', '123456', NULL, NULL, NULL), 
+(NULL, 'Widgets', '549 Elm Street', '', 'Denver', 'CO', '80234', 'United States', 'Douglas', 'Widgets', '13768 Third Ave.', '', 'Denver', 'CO', '80205', 'United States', 'Jackson', 'Pete', 'Mitchell', '3035551212', 'pm@google.com', 'Mike', 'Smith', '3035551212', 'ms@google.com', 'Water Bulk,Fuel,Professional Services', NULL, NULL, '31241243123413', '123456', NULL, NULL, NULL), 
+(NULL, 'My Company', '8959 My Street', '', 'Denver', 'CO', '80206', 'United States', 'Denver', 'My Company', '3245 Ohio Ave.', '', 'Denver', 'CO', '80207', 'United States', 'Washington', 'Mark', 'Hanson', '3035551212', 'mh@google.com', 'Sheila', 'Johnson', '3035551212', 'sj@google.com', 'Water Bottled,Showers,Temporary Facilities', NULL, NULL, '2547647245652465', '222222', NULL, NULL, NULL),
+(5, 'Showers R Us', '6544 Cleveland Drive', '', 'Denver', 'CO', '80204', 'United States', 'Douglas', 'Showers R Us', '6544 Cleveland Drive', '', 'Denver', 'CO', '80204', 'United States', 'Douglas', 'Hank', 'Shelton', '3037777777', 'hank@google.com', 'Kevin', 'Johnson', '3038886565', 'kevin@google.com', 'Showers', '', NULL, '833240860512492791cabfefbf2d10d8', '123456', NULL, NULL, NULL)
---
+++ INSERT_Services.sql
@@ -0,0 +1,18 @@
+INSERT INTO app.service(
+   id
+  ,name
+  ,shortname
+  ,comment
+) VALUES (NULL, 'Water Bottled', 'Water Bottled', ''),
+(NULL, 'Water Bulk', 'Water Bulk', ''),
+(NULL, 'Sanitation/Toilet/Sink', 'Sanitation/Toilet/Sink', ''),
+(NULL, 'Dumpsters', 'Dumpsters', ''),
+(NULL, 'Showers', 'Showers', ''),
+(NULL, 'Generators', 'Generators', ''),
+(NULL, 'Pumps', 'Pumps', ''),
+(NULL, 'Heavy Equipment', 'Heavy Equipment', ''),
+(NULL, 'Fuel', 'Fuel', ''),
+(NULL, 'Sandbags', 'Sandbags', ''),
+(NULL, 'Temporary Facilities', 'Temporary Facilities', ''),
+(NULL, 'Professional Services', 'Professional Services', ''),
+(NULL, 'Other', 'Other', '')
